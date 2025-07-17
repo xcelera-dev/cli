28a75dd63f84eabcca841272abe0c29eae8e2cdc
Zac <zac@xcelera.dev>
2025-07-18 10:49:59 +1200

    hello cli

--- rollup.config.ts
+++ rollup.config.ts
@@ -4,15 +4,27 @@ import commonjs from '@rollup/plugin-commonjs'
 import nodeResolve from '@rollup/plugin-node-resolve'
 import typescript from '@rollup/plugin-typescript'
 
-const config = {
-  input: 'src/index.ts',
-  output: {
-    esModule: true,
-    file: 'dist/index.js',
-    format: 'es',
-    sourcemap: true
+const config = [
+  {
+    input: 'src/index.ts',
+    output: {
+      esModule: true,
+      file: 'dist/index.js',
+      format: 'es',
+      sourcemap: true
+    },
+    plugins: [typescript(), nodeResolve({ preferBuiltins: true }), commonjs()]
   },
-  plugins: [typescript(), nodeResolve({ preferBuiltins: true }), commonjs()]
-}
+  {
+    input: 'src/cli.ts',
+    output: {
+      esModule: true,
+      file: 'dist/cli.js',
+      format: 'es',
+      sourcemap: true
+    },
+    plugins: [typescript(), nodeResolve({ preferBuiltins: true }), commonjs()]
+  }
+]
 
 export default config
